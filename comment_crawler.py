e78adf60ad38c2eda75b7a73c2b5ca4690b53c63
FerMorales <63946989+F3rM0ral3s@users.noreply.github.com>
2024-03-14 19:39:41 -0600

    Se agrega likes, fecha y nuevo formato xlxs

--- comment_crawler.py
+++ comment_crawler.py
@@ -1,38 +1,126 @@
 from selenium.webdriver.common.by import By
-from selenium.webdriver.common.action_chains import ActionChains
-import time
+from openpyxl import Workbook
+from openpyxl import load_workbook
+from time import sleep
+import winsound
+import re
+import datetime
 
-def getComments(driver):
+
+def createExcel():
+    excel = Workbook()
+    hoja = excel.active
+
+    # Headers de columna
+    headers = ["Descripción","URL","Fecha Publicación","Comentario","Likes"]
+
+    # Imprimimos en el excel
+    for col,head in enumerate(headers):
+         hoja.cell(row=1,column=col+1,value=head)
+
+    # Fecha de analisis
+    hoja.cell(row=1,column=7,value='Fecha de analisis:')
+    hoja.cell(row=1,column=8,value=datetime.date.today())
+
+    excel.save("videos_comments.xlsx")
+
+def getComments(driver,url,desc,other_info):
+
+    #Inicializamos lista de comentarios
+    coms = []
+
+    # Accedemos al excel
+    excel = load_workbook("videos_comments.xlsx")
+    hoja = excel.active
+    curr = hoja.max_row + 1
+
     try:
-        coms = []
-        # Encuentra todos los comentarios en ventana
+        # Escribimos la descripcion, la url y tratamos la fecha
+        hoja.cell(row=curr,column=1,value=desc)
+        hoja.cell(row=curr,column=2,value=url)
+        hoja.cell(row=curr,column=3,value=re.split('\n',other_info)[2])
+        
+        # Encuentra la ventan de comentarios
         comment_section = driver.find_element(By.CSS_SELECTOR,'div.css-1qp5gj2-DivCommentListContainer.ekjxngi3')
-        ant = 0
-        cont=0 
-        for i in range(1,15):
-            coms = driver.find_elements(By.XPATH,"//span[@dir]")
-            tam = len(coms)
-            if(ant==tam):
-                cont+=1
-            else:
-                cont=0
-            ant=tam
-            if(cont==3):
-                break
-            driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + {}".format(5000*i), comment_section)
-            time.sleep(0.4)
+        
+        # Variables auxiliares
+        start = 0
+        i=1
 
-        with open('comments.txt','a', encoding='utf-8') as archivo:
+        # Encuentra todos los comentarios visibles
+        coms = driver.find_elements(By.CSS_SELECTOR,'div.css-ulyotp-DivCommentContentContainer.e1g2efjf0')
+        while(start!=len(coms)):
+            # Acotamos los comments a guardar
+            if(len(coms)>=500):
+                 break
+            
+            # Acotamos la lista de comentarios
+            coms = coms[start:]
+
+            # Escribimos los comments
             for com in coms:
-                archivo.write(com.text+'\n')
+                # Tenemos que hacerlo ligado para estar seguro que la cantidad
+                # de likes y el texto corresponden al mismo comentario
+
+                # Obtenemos el texto
+                texto = com.find_element(By.CSS_SELECTOR,'div.css-1mf23fd-DivContentContainer.e1g2efjf1')
+                texto = texto.find_element(By.CSS_SELECTOR,'p.css-xm2h10-PCommentText.e1g2efjf6')
+                
+                # Obtenemos numero de likes
+                likes = com.find_element(By.CSS_SELECTOR,'div.css-1swe2yf-DivActionContainer.esns4rh0')
+                likes = likes.find_element(By.CSS_SELECTOR,'div.css-114tc9h-DivLikeWrapper.ezxoskx0')
+                likes = likes.get_attribute('aria-label')
+            
+                # Escribimos el texto y likes
+                hoja.cell(row=curr,column=4,value=texto.text)
+                hoja.cell(row=curr,column=5,value=re.findall('\d+',likes)[0])
+                curr+=1
+                start+=1
+
+            # Hacemos scroll down
+            driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + {}".format(2500*i), comment_section)
+            i+=1
+            sleep(1)
+
+            #Buscamos si apareció un captcha
+            try:
+                driver.find_element(By.CSS_SELECTOR,"div.captcha_verify_container.style__CaptchaWrapper-sc-1gpeoge-0.zGYIR") 
+                
+                # Esperamos respuesta del usuario
+                winsound.Beep(440,2500)
+                response = input('[COMMENTS] Resolviste el captcha [y/n]: ')
+                if(response not in ['y','Y']):
+                    driver.quit()
+
+            except Exception as e:
+                 pass
+
+            # Buscamos nuevos comments
+            coms = driver.find_elements(By.CSS_SELECTOR,'div.css-ulyotp-DivCommentContentContainer.e1g2efjf0')
+            
+        # Guardamos
+        excel.save("videos_comments.xlsx")   
 
     except Exception as e:
         print('[ Error en comments ]')
-        with open('comments.txt','a', encoding='utf-8') as archivo:
-            for com in coms:
-                archivo.write(com.text+'\n')
+        print('[ Se salvaron %d comentarios ]' % len(coms))
+        print(e)
+        for com in coms:
+                # Tenemos que hacerlo ligado para estar seguro que la cantidad
+                # de likes y el texto corresponden al mismo comentario
+
+                # Obtenemos el texto
+                texto = com.find_element(By.CSS_SELECTOR,'div.css-1mf23fd-DivContentContainer.e1g2efjf1')
+                texto = texto.find_element(By.CSS_SELECTOR,'p.css-xm2h10-PCommentText.e1g2efjf6')
                 
-
-        
-
-    
+                # Obtenemos numero de likes
+                likes = com.find_element(By.CSS_SELECTOR,'div.css-1swe2yf-DivActionContainer.esns4rh0')
+                likes = likes.find_element(By.CSS_SELECTOR,'div.css-114tc9h-DivLikeWrapper.ezxoskx0')
+                likes = likes.get_attribute('aria-label')
+            
+                # Escribimos el texto y likes
+                hoja.cell(row=curr,column=4,value=texto.text)
+                hoja.cell(row=curr,column=5,value=re.findall('\d+',likes)[0])
+                curr+=1
+                start+=1  
+        excel.save("videos_comments.xlsx")          
--- crawler.py
+++ crawler.py
@@ -2,7 +2,7 @@ from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.action_chains import ActionChains
-import time
+from time import sleep
 import winsound
 
 import comment_crawler
@@ -12,23 +12,20 @@ options = Options()
 options.add_argument("--disable-notifications") 
 driver = webdriver.Chrome(options=options)
 
+# Creamos excel
+comment_crawler.createExcel()
+
 # Palabras clave a buscar
 keywords = ['Elecciones 2024 Mexico','Sheinbaum','Xochitl Galvez','Morena amlo']
 
-# Se escribe la cabecera de la busqueda en el txt
-with open('urls.txt','w') as archivo:
-   archivo.write(keywords[0]+'\n')
-
-# Iniciamos txt de comments
-with open('comments.txt','w') as archivo:
-   archivo.write('-----------------COMMENTS------------------\n')   
-
+# Inicializamos las acciones del navegador y una variable sumador
 actions = ActionChains(driver)
 sum=0
+
 for key in keywords:
   # Lanzamos el navegador
   driver.get('https://www.tiktok.com')
-  time.sleep(3)
+  sleep(3)
 
   # Iniciar sesión como invitado
   try:
@@ -36,15 +33,16 @@ for key in keywords:
     login_scren.click()
   except:
     print('[[No se encontó login screen]]')
+  sleep(1)
 
-
-  time.sleep(1)
+  
   # Se busca videos relacionados con las elecciones de 2024
   search =  driver.find_element(By.CSS_SELECTOR,'input.css-1yf5w3n-InputElement.e14ntknm3')
-  
   search.send_keys(key)
   search.submit()
-  time.sleep(4)
+  sleep(5)
+
+  print('Buscando %s' % key)
 
   # Videos
   video = driver.find_element(By.CSS_SELECTOR,'div.css-1soki6-DivItemContainerForSearch.e19c29qe10')
@@ -53,21 +51,22 @@ for key in keywords:
   '''
     SI TODO SALIO BIEN EN ESTE MOMENTO TENDRAS QUE RESOLVER UN CAPTCHA
   '''
+
   # Esperamos respuesta del usuario
-  response = input('Resolviste el captcha [y/n]: ')
+  response = input('[VIDEO] Resolviste el captcha [y/n]: ')
   if(response not in ['y','Y']):
     driver.quit()
 
-  # Inicializamos lista de urls
-  urls = []
-  ########################
-  time.sleep(1)
-  aux=driver
-  comment_crawler.getComments(driver)
-  driver=aux
-  ########################
-  urls.append(driver.current_url)
+  # Guardamos la url del tiktok
+  url = driver.current_url
   sum+=1
+
+ 
+
+  # Capturamos el primer batch de comentarios
+  desc = driver.find_element(By.CSS_SELECTOR,'span.css-j2a19r-SpanText.efbd9f0')
+  other_info = driver.find_element(By.CSS_SELECTOR,'span.css-gg0x0w-SpanOtherInfos.evv7pft3')
+  comment_crawler.getComments(driver,url,desc.text,other_info.text)
   driver.back()
 
   try:
@@ -75,41 +74,54 @@ for key in keywords:
     # Encuentra todos los placeholder de los videos en ventana
     videos = driver.find_elements(By.CSS_SELECTOR,'div.css-1soki6-DivItemContainerForSearch.e19c29qe10')
     for i in range(100):
+
+      # Esta lista es de los tiktoks en pantalla que no hemos analizado
       videos = videos[start:]
+
       for vid in videos:
-          print('- Links obtenidos %d' % sum)
-          time.sleep(1.5)
+          # Printeamos el num de videos analizados
+          # y entramos al tiktok
+          print('- Videos analizados %d' % sum)
+          sleep(1.5)
           vid.click()
-          time.sleep(1)
-          ########################
+          sleep(2)
+          
+          # Obtenemos batch de comentarios
           aux=driver
-          comment_crawler.getComments(driver)
+          url = driver.current_url
+          desc = driver.find_element(By.XPATH,'//div[@data-e2e="browse-video-desc"]')
+          other_info = driver.find_element(By.CSS_SELECTOR,'span.css-gg0x0w-SpanOtherInfos.evv7pft3')
+          comment_crawler.getComments(driver,url,desc.text,other_info.text)
           driver=aux
-          ########################
-          urls.append(driver.current_url)
+          
           sum+=1
-          time.sleep(0.5)
-          driver.back()
           start+=1
+          driver.back()
+
       # Busca mas placeholder dando scrolldown
       actions.scroll_by_amount(0, 4000).perform()
-      time.sleep(1.5)
+      sleep(1.5)
+      try:
+        driver.find_element(By.CSS_SELECTOR,"div.captcha_verify_container.style__CaptchaWrapper-sc-1gpeoge-0.zGYIR") 
+                
+        # Esperamos respuesta del usuario
+        winsound.Beep(440,2500)
+        response = input('[VIDEOS] Resolviste el captcha [y/n]: ')
+        if(response not in ['y','Y']):
+            driver.quit()
+
+      except Exception as e:
+        pass
+
       videos = driver.find_elements(By.CSS_SELECTOR,'div.css-1soki6-DivItemContainerForSearch.e19c29qe10')
+
+
   except Exception as e:
     print(e)
 
-    # Guardamos las URLs obtenidas
-    with open('urls.txt', 'a') as archivo:
-        archivo.write('Buscando '+key+'\n')
-        for url in urls:
-            archivo.write(url+ "\n")
-    
-    # Nos encontramos con un Captcha, recargamos la página
-    winsound.Beep(1800,2500)
-    
 
-# Total de links obtenidos
-print('[Links obtenidos %d]' % sum)
+# Total de videos y comments
+print('[Total de videos analizados %d]' % sum)
 
 # Cerramos navegador
 driver.quit()
