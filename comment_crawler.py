8c4d5d17c173ecf16fca8dc3cb93219ee5c29b6b
FerMorales <63946989+F3rM0ral3s@users.noreply.github.com>
2024-05-14 01:54:59 -0600

    Add files via upload

--- comment_crawler.py
+++ comment_crawler.py
@@ -4,7 +4,7 @@ from openpyxl import load_workbook
 from time import sleep
 import winsound
 import re
-import datetime
+from datetime import datetime,timedelta
 
 
 def createExcel():
@@ -12,23 +12,52 @@ def createExcel():
     hoja = excel.active
 
     # Headers de columna
-    headers = ["Descripción","URL","Fecha Publicación","Comentario","Likes"]
+    headers = ["Descripción","URL","Fecha Publicación","Comentario","Likes","Fecha comment"]
 
     # Imprimimos en el excel
     for col,head in enumerate(headers):
          hoja.cell(row=1,column=col+1,value=head)
 
     # Fecha de analisis
-    hoja.cell(row=1,column=7,value='Fecha de analisis:')
-    hoja.cell(row=1,column=8,value=datetime.date.today())
+    hoja.cell(row=1,column=8,value='Fecha de analisis:')
+    hoja.cell(row=1,column=9,value=datetime.now())
 
     excel.save("videos_comments.xlsx")
 
-def getComments(driver,url,desc,other_info):
+def getFecha(fecha_str):
+    try:
+        if 'Hace' in fecha_str:
+            num = int(re.findall('\d+',fecha_str)[0])
+            if 'segundo' in fecha_str:
+                return datetime.now()
+            elif 'min' in fecha_str:
+                return datetime.now()
+            elif ('hora' in fecha_str) or ('h' in fecha_str):
+                return datetime.now()
+            elif 'día' in fecha_str:
+                return datetime.now()-timedelta(days=num)
+            elif 'semana' in fecha_str:
+                return datetime.now()-timedelta(weeks=num)
+            elif 'mes' in fecha_str:
+                return datetime.now()-timedelta(months=num)
+            else:
+                return fecha_str
 
+        else:
+            nums = re.split('-',fecha_str)
+            if(len(nums)==2):
+                return datetime(2024,int(nums[0]),int(nums[1]))
+            else:
+                return datetime(int(nums[2]),int(nums[0]),int(nums[1]))
+    except Exception as e:
+        print(e)
+        print(' [ ERROR EN FECHA ]')
+        return fecha_str
+
+def getComments(driver,url,desc,other_info):
     #Inicializamos lista de comentarios
     coms = []
-
+   
     # Accedemos al excel
     excel = load_workbook("videos_comments.xlsx")
     hoja = excel.active
@@ -38,27 +67,21 @@ def getComments(driver,url,desc,other_info):
         # Escribimos la descripcion, la url y tratamos la fecha
         hoja.cell(row=curr,column=1,value=desc)
         hoja.cell(row=curr,column=2,value=url)
-        hoja.cell(row=curr,column=3,value=re.split('\n',other_info)[2])
-        
-        # Encuentra la ventan de comentarios
-        comment_section = driver.find_element(By.CSS_SELECTOR,'div.css-1qp5gj2-DivCommentListContainer.ekjxngi3')
+        hoja.cell(row=curr,column=3,value=getFecha(re.split('\n',other_info)[2]))
         
         # Variables auxiliares
         start = 0
-        i=1
 
         # Encuentra todos los comentarios visibles
         coms = driver.find_elements(By.CSS_SELECTOR,'div.css-ulyotp-DivCommentContentContainer.e1g2efjf0')
-        while(start!=len(coms)):
-            # Acotamos los comments a guardar
-            if(len(coms)>=500):
-                 break
-            
+        while(True):
             # Acotamos la lista de comentarios
+            aux = coms
             coms = coms[start:]
-
+    
             # Escribimos los comments
             for com in coms:
+                driver.execute_script("arguments[0].scrollIntoView(true);", com)
                 # Tenemos que hacerlo ligado para estar seguro que la cantidad
                 # de likes y el texto corresponden al mismo comentario
 
@@ -70,33 +93,36 @@ def getComments(driver,url,desc,other_info):
                 likes = com.find_element(By.CSS_SELECTOR,'div.css-1swe2yf-DivActionContainer.esns4rh0')
                 likes = likes.find_element(By.CSS_SELECTOR,'div.css-114tc9h-DivLikeWrapper.ezxoskx0')
                 likes = likes.get_attribute('aria-label')
-            
-                # Escribimos el texto y likes
+
+                # Obtenemos la fecha del comment
+                fecha_com = com.find_element(By.CSS_SELECTOR,'div.css-1mf23fd-DivContentContainer.e1g2efjf1')
+                fecha_com = fecha_com.find_element(By.CSS_SELECTOR,'span.css-4tru0g-SpanCreatedTime.e1g2efjf8')
+                
+                # Escribimos el texto, likes y fecha de comment
                 hoja.cell(row=curr,column=4,value=texto.text)
                 hoja.cell(row=curr,column=5,value=re.findall('\d+',likes)[0])
+                hoja.cell(row=curr,column=6,value=getFecha(fecha_com.text))
                 curr+=1
                 start+=1
 
-            # Hacemos scroll down
-            driver.execute_script("arguments[0].scrollTop = arguments[0].scrollTop + {}".format(2500*i), comment_section)
-            i+=1
-            sleep(1)
+                #Buscamos si apareció un captcha
+                try:
+                    driver.find_element(By.CSS_SELECTOR,"div.captcha_verify_container.style__CaptchaWrapper-sc-1gpeoge-0.zGYIR") 
+        
+                    # Esperamos respuesta del usuario
+                    winsound.Beep(440,2000)
+                    response = input('[COMMENTS] Resolviste el captcha [y/n]: ')
+                    if(response not in ['y','Y']):
+                        driver.quit()
 
-            #Buscamos si apareció un captcha
-            try:
-                driver.find_element(By.CSS_SELECTOR,"div.captcha_verify_container.style__CaptchaWrapper-sc-1gpeoge-0.zGYIR") 
-                
-                # Esperamos respuesta del usuario
-                winsound.Beep(440,2500)
-                response = input('[COMMENTS] Resolviste el captcha [y/n]: ')
-                if(response not in ['y','Y']):
-                    driver.quit()
-
-            except Exception as e:
-                 pass
+                except Exception as e:
+                    pass
 
             # Buscamos nuevos comments
+            sleep(2)
             coms = driver.find_elements(By.CSS_SELECTOR,'div.css-ulyotp-DivCommentContentContainer.e1g2efjf0')
+            if(len(coms)==len(aux) or start >=500):
+                break
             
         # Guardamos
         excel.save("videos_comments.xlsx")   
@@ -117,10 +143,15 @@ def getComments(driver,url,desc,other_info):
                 likes = com.find_element(By.CSS_SELECTOR,'div.css-1swe2yf-DivActionContainer.esns4rh0')
                 likes = likes.find_element(By.CSS_SELECTOR,'div.css-114tc9h-DivLikeWrapper.ezxoskx0')
                 likes = likes.get_attribute('aria-label')
-            
-                # Escribimos el texto y likes
+
+                # Obtenemos la fecha del comment
+                fecha_com = com.find_element(By.CSS_SELECTOR,'div.css-1mf23fd-DivContentContainer.e1g2efjf1')
+                fecha_com = fecha_com.find_element(By.CSS_SELECTOR,'span.css-4tru0g-SpanCreatedTime.e1g2efjf8')
+                
+                # Escribimos el texto, likes y fecha de comment
                 hoja.cell(row=curr,column=4,value=texto.text)
                 hoja.cell(row=curr,column=5,value=re.findall('\d+',likes)[0])
+                hoja.cell(row=curr,column=6,value=getFecha(fecha_com.text))
                 curr+=1
-                start+=1  
+                start+=1
         excel.save("videos_comments.xlsx")          
