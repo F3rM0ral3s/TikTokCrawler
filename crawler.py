52731e5399fa756535c46a540b973c285bd34661
FerMorales <63946989+F3rM0ral3s@users.noreply.github.com>
2024-05-14 01:55:08 -0600

    Add files via upload

--- crawler.py
+++ crawler.py
@@ -20,7 +20,7 @@ keywords = ['Elecciones 2024 Mexico','Sheinbaum','Xochitl Galvez','Morena amlo']
 
 # Inicializamos las acciones del navegador y una variable sumador
 actions = ActionChains(driver)
-sum=0
+sum=1
 
 for key in keywords:
   # Lanzamos el navegador
@@ -35,7 +35,7 @@ for key in keywords:
     print('[[No se encontó login screen]]')
   sleep(1)
 
-  
+
   # Se busca videos relacionados con las elecciones de 2024
   search =  driver.find_element(By.CSS_SELECTOR,'input.css-1yf5w3n-InputElement.e14ntknm3')
   search.send_keys(key)
@@ -44,33 +44,8 @@ for key in keywords:
 
   print('Buscando %s' % key)
 
-  # Videos
-  video = driver.find_element(By.CSS_SELECTOR,'div.css-1soki6-DivItemContainerForSearch.e19c29qe10')
-  video.click()
-
-  '''
-    SI TODO SALIO BIEN EN ESTE MOMENTO TENDRAS QUE RESOLVER UN CAPTCHA
-  '''
-
-  # Esperamos respuesta del usuario
-  response = input('[VIDEO] Resolviste el captcha [y/n]: ')
-  if(response not in ['y','Y']):
-    driver.quit()
-
-  # Guardamos la url del tiktok
-  url = driver.current_url
-  sum+=1
-
- 
-
-  # Capturamos el primer batch de comentarios
-  desc = driver.find_element(By.CSS_SELECTOR,'span.css-j2a19r-SpanText.efbd9f0')
-  other_info = driver.find_element(By.CSS_SELECTOR,'span.css-gg0x0w-SpanOtherInfos.evv7pft3')
-  comment_crawler.getComments(driver,url,desc.text,other_info.text)
-  driver.back()
-
   try:
-    start = 1
+    start = 0
     # Encuentra todos los placeholder de los videos en ventana
     videos = driver.find_elements(By.CSS_SELECTOR,'div.css-1soki6-DivItemContainerForSearch.e19c29qe10')
     for i in range(100):
@@ -82,9 +57,24 @@ for key in keywords:
           # Printeamos el num de videos analizados
           # y entramos al tiktok
           print('- Videos analizados %d' % sum)
-          sleep(1.5)
+          sleep(3)
+          driver.execute_script("arguments[0].scrollIntoView(true);", vid)
+          sleep(1)
           vid.click()
-          sleep(2)
+          sleep(2.5)
+
+          # Buscamos captcha
+          try:
+            driver.find_element(By.CSS_SELECTOR,"div.captcha_verify_container.style__CaptchaWrapper-sc-1gpeoge-0.zGYIR") 
+            
+            # Esperamos respuesta del usuario
+            winsound.Beep(440,2000)
+            response = input('[VIDEOS] Resolviste el captcha [y/n]: ')
+            if(response not in ['y','Y']):
+                driver.quit()
+
+          except Exception as e:
+            pass
           
           # Obtenemos batch de comentarios
           aux=driver
@@ -97,21 +87,6 @@ for key in keywords:
           sum+=1
           start+=1
           driver.back()
-
-      # Busca mas placeholder dando scrolldown
-      actions.scroll_by_amount(0, 4000).perform()
-      sleep(1.5)
-      try:
-        driver.find_element(By.CSS_SELECTOR,"div.captcha_verify_container.style__CaptchaWrapper-sc-1gpeoge-0.zGYIR") 
-                
-        # Esperamos respuesta del usuario
-        winsound.Beep(440,2500)
-        response = input('[VIDEOS] Resolviste el captcha [y/n]: ')
-        if(response not in ['y','Y']):
-            driver.quit()
-
-      except Exception as e:
-        pass
 
       videos = driver.find_elements(By.CSS_SELECTOR,'div.css-1soki6-DivItemContainerForSearch.e19c29qe10')
 
